e5cae60ba9059e32fa65e9be2ba03b684912263e
Matt Pryor <matt.pryor@stfc.ac.uk>
2017-09-20 09:40:16 +0100

    Refactor build process

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/main.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
     },
     module: {
         rules: [
@@ -26,8 +28,13 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             title: 'JASMIN Cloud Portal',
-            template: 'index.template.html',
+            template: 'assets/index.template.html',
             hash: true
-        })
+        }),
+        new CopyWebpackPlugin([
+            { context: 'assets', from: 'jasmin_theme/css', to: 'jasmin_theme/css' },
+            { context: 'assets', from: 'jasmin_theme/img', to: 'jasmin_theme/img' },
+            { context: 'assets', from: 'tweaks.css' }
+        ])
     ]
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,18 +1,19 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+
 
 module.exports = merge(
     require('./webpack.common.js'),
     {
         devtool: 'source-map',
         plugins: [
-            new UglifyJSPlugin({ sourceMap: true }),
             new webpack.DefinePlugin({
                 'process.env': {
                     'NODE_ENV': JSON.stringify('production')
                 }
-            })
+            }),
+            new UglifyJSPlugin({ sourceMap: true })
         ]
     }
 );
