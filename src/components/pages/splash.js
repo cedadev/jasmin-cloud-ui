676cda11b8af3d61b58b99811e6942ad98e3a553
Matt Pryor <matt.pryor@stfc.ac.uk>
2017-09-21 11:18:47 +0100

    Improved splash page

--- src/components/pages/splash.js
+++ src/components/pages/splash.js
@@ -3,6 +3,8 @@
  */
 
 import React from 'react';
+import { Row, Col, Well, Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 
 export class SplashPage extends React.Component {
@@ -12,7 +14,40 @@ export class SplashPage extends React.Component {
 
     render() {
         return (
-            <h1 className="page-header">This is the splash page</h1>
+            <Row>
+                <Col md={12}>
+                    <div className="splash">
+                        <div className="splash-header">
+                            <h1>Welcome to the JASMIN Cloud Portal</h1>
+                        </div>
+                        <div className="splash-body clearfix">
+                            <Col md={7}>
+                                <p>Here, you can manage your tenancies in the JASMIN Cloud.</p>
+                            </Col>
+                            <Col md={5}>
+                                <Well>
+                                    <LinkContainer to={`/dashboard`}>
+                                        <Button bsStyle="primary" bsSize="lg" block>
+                                            <i className="fa fa-fw fa-cloud" />
+                                            {'\u00A0\u00A0'}
+                                            My JASMIN Cloud tenancies
+                                        </Button>
+                                    </LinkContainer>
+                                    <Button
+                                      bsStyle="primary"
+                                      bsSize="lg"
+                                      block
+                                      href="https://accounts.jasmin.ac.uk/services/cloud_tenancies/">
+                                        <i className="fa fa-fw fa-user-plus" />
+                                        {'\u00A0\u00A0'}
+                                        Join a JASMIN Cloud tenancy
+                                    </Button>
+                                </Well>
+                            </Col>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
         );
     }
 }
