c333453fa5ae5adf36c4e6a3493e4ea25fbc53f3
Matt Pryor <matt.pryor@stfc.ac.uk>
2020-04-14 10:14:15 +0100

    Add support for the webpack dev server

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,15 @@
 const merge = require('webpack-merge');
+const path = require('path');
 
 module.exports = merge(
     require('./webpack.common.js'),
     {
-        devtool: 'inline-source-map'
+        devtool: 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'dist'),
+            compress: true,
+            port: 9000,
+            disableHostCheck: true
+        }
     }
 );
